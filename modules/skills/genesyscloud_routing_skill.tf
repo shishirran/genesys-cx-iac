7ac462efd39751f26ee67d3ef781662b6d272d57
Shishir <shishirran@gmail.comr>
2026-06-07 20:48:45 +0530

    changes on 07June 2048

---
+++ modules/skills/genesyscloud_routing_skill.tf
@@ -0,0 +1,12 @@
+resource "genesyscloud_routing_skill" "Mortgage_1" {
+  name = "MortgageSkill"
+}
+
+resource "genesyscloud_routing_skill" "Mortgage_2" {
+  name = "MortgageSkill2"
+}
+
+resource "genesyscloud_routing_skill" "Mortgage_3" {
+  name = "MortgageSkill3"
+}
+
